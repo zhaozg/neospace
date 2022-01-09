8c2f3eea46960afcd6cad0e76d65a6c2f9dd99be
zhaozg <zhaozg@gmail.com>
2022-01-09 14:05:53 +0800

    update tabline

--- layers/ui.lua
+++ layers/ui.lua
@@ -53,12 +53,14 @@ return {
       tabline.setup {
         enable = false,
         options = {
-        -- If lualine is installed tabline will use separators configured in lualine by default.
-        -- These options can be used to override those settings.
           section_separators = {'', ''},
           component_separators = {'', ''},
           max_bufferline_percent = 66, -- set to nil by default, and it uses vim.o.columns * 2/3
-          always_show_tabs = false, -- by default, this shows tabs only when there are more than one tab or if the first tab is named
+          show_last_separator = false,
+          show_tabs_always = false,
+          show_bufnr = true,
+          show_devicons = true,
+          show_filename_only = true
         }
       }
     end
