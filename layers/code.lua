e0985296cc9ba28e87eb78ba7406f808c8983d5d
zhaozg <zhaozg@gmail.com>
2022-03-04 21:15:45 +0800

    use Comment.nvim replace nvim-comment

--- layers/code.lua
+++ layers/code.lua
@@ -3,25 +3,9 @@ local g = vim.g
 
 return {
   {
-    "terrortylor/nvim-comment",
-    after = "JoosepAlviste/nvim-ts-context-commentstring",
+    'numToStr/Comment.nvim',
     config = function()
-      require("nvim_comment").setup({
-        -- Linters prefer comment and line to have a space in between markers
-        marker_padding = true,
-        -- should comment out empty or whitespace only lines
-        comment_empty = false,
-        -- Should key mappings be created
-        create_mappings = true,
-        -- Normal mode mapping left hand side
-        line_mapping = "<leader>c ",
-        -- Visual/Operator mapping left hand side
-        operator_mapping = "<leader>cc",
-        -- Hook function to call before commenting takes place
-        hook = function()
-          require("ts_context_commentstring.internal").update_commentstring()
-        end,
-      })
+      require('Comment').setup()
     end,
   },
   {
