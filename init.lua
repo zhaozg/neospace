471b2e09066904934f6c8d1ccd97578a382cccee
zhaozg <zhaozg@gmail.com>
2022-03-11 10:52:10 +0800

    update keymap for neovide

--- init.lua
+++ init.lua
@@ -20,8 +20,17 @@ if vim.fn.exists("g:neovide") then
   vim.g.neovide_fullscreen = true
   vim.g.neovide_cursor_antialiasing = false
   vim.g.neovide_cursor_vfx_mode = "railgun"
+  vim.g.neovide_input_use_logo=true
 
   vim.cmd("set clipboard+=unnamedplus")
+
+  -- system clipboard
+  vim.keymap.set('',  '<D-c>', '"+y')
+  vim.keymap.set('n', '<D-v>', '"+p')
+  vim.cmd("inoremap <D-v> <c-r>+")
+  vim.cmd("cnoremap <D-v> <c-r>+")
+  --use <c-r> to insert original character without triggering things like auto-pairs
+  vim.cmd("inoremap <D-r> <c-v>")
 end
 
 if vim.fn.filereadable('.nvimrc')==1 then
