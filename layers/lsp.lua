2fdf390f3110678bbf49deccdd3e9a9b691a0369
zhaozg <zhaozg@gmail.com>
2022-04-30 23:28:13 +0800

    update lsp layer

--- layers/lsp.lua
+++ layers/lsp.lua
@@ -34,89 +34,12 @@ return {
     'nvim-lua/lsp-status.nvim',
   },
   {
-    'ojroques/nvim-lspfuzzy',
-    config = function()
-      require('lspfuzzy').setup {}
-    end
-  },
-  {
     "tami5/lspsaga.nvim",
-    init = function()
-      vim.cmd([[
-      "-- lsp provider to find the cursor word definition and reference
-      nnoremap <silent> gh :Lspsaga lsp_finder<CR>
-
-      "-- code action
-      nnoremap <silent><leader>ca :Lspsaga code_action<CR>
-      vnoremap <silent><leader>ca :<C-U>Lspsaga range_code_action<CR>
-
-      "-- show hover doc
-      nnoremap <silent>K :Lspsaga hover_doc<CR>
-
-      "-- scroll down hover doc or scroll in definition preview
-      nnoremap <silent> <C-f> <cmd>lua require('lspsaga.action').smart_scroll_with_saga(1)<CR>
-      "- scroll up hover doc
-      nnoremap <silent> <C-b> <cmd>lua require('lspsaga.action').smart_scroll_with_saga(-1)<CR>
-
-      "-- show signature help
-      nnoremap <silent> gs :Lspsaga signature_help<CR>
-
-      "-- rename
-      nnoremap <silent>gr :Lspsaga rename<CR>
-
-      "-- preview definition
-      nnoremap <silent> gd :Lspsaga preview_definition<CR>
-
-      "-- Jump Diagnostic and Show Diagnostics
-      "-- show
-      nnoremap <silent> <leader>cd :Lspsaga show_line_diagnostics<CR>
-
-      "-- only show diagnostic if cursor is over the area
-      nnoremap <silent><leader>cc <cmd>lua require'lspsaga.diagnostic'.show_cursor_diagnostics()<CR>
-
-      "-- jump diagnostic
-      nnoremap <silent> [e :Lspsaga diagnostic_jump_next<CR>
-      nnoremap <silent> ]e :Lspsaga diagnostic_jump_prev<CR>
-      ]])
-    end,
     config = function()
       local saga = require 'lspsaga'
       saga.init_lsp_saga({
         use_saga_diagnostic_sign = false,
-        -- add your config value here
-        -- default value
-        -- error_sign = '',
-        -- warn_sign = '',
-        -- hint_sign = '',
-        -- infor_sign = '',
-        -- dianostic_header_icon = '   ',
-        -- code_action_icon = ' ',
-        -- code_action_prompt = {
-        --   enable = true,
-        --   sign = true,
-        --   sign_priority = 20,
-        --   virtual_text = true,
-        -- },
-        -- finder_definition_icon = '  ',
-        -- finder_reference_icon = '  ',
-        -- max_preview_lines = 10, -- preview lines of lsp_finder and definition preview
-        -- finder_action_keys = {
-        --   open = 'o', vsplit = 's',split = 'i',quit = 'q',scroll_down = '<C-f>', scroll_up = '<C-b>' -- quit can be a table
-        -- },
-        -- code_action_keys = {
-        --   quit = 'q',exec = '<CR>'
-        -- },
-        -- rename_action_keys = {
-        --   quit = '<C-c>',exec = '<CR>'  -- quit can be a table
-        -- },
-        -- definition_preview_icon = '  '
-        -- "single" "double" "round" "plus"
-        -- border_style = "single"
-        -- rename_prompt_prefix = '➤',
-        -- if you don't use nvim-lspconfig you must pass your server name and
-        -- the related filetypes into this table
-        -- like server_filetype_map = {metals = {'sbt', 'scala'}}
-        -- server_filetype_map = {}
+        code_action_icon = '',
       })
     end
   }
--- lua/neospace/lsp.lua
+++ lua/neospace/lsp.lua
@@ -1,35 +1,59 @@
 local vim = vim
 
--- Mappings.
--- See `:help vim.diagnostic.*` for documentation on any of the below functions
+local map = vim.api.nvim_buf_set_keymap
 local opts = { noremap=true, silent=true }
-
-vim.api.nvim_set_keymap('n', '<space>e', '<cmd>lua vim.diagnostic.open_float()<CR>', opts)
-vim.api.nvim_set_keymap('n', '[d', '<cmd>lua vim.diagnostic.goto_prev()<CR>', opts)
-vim.api.nvim_set_keymap('n', ']d', '<cmd>lua vim.diagnostic.goto_next()<CR>', opts)
-vim.api.nvim_set_keymap('n', '<space>q', '<cmd>lua vim.diagnostic.setloclist()<CR>', opts)
 
 -- Use an on_attach function to only map the following keys
 -- after the language server attaches to the current buffer
 local on_attach = function(client, bufnr)
+  _ = client
+
   -- Enable completion triggered by <c-x><c-o>
   vim.api.nvim_buf_set_option(bufnr, 'omnifunc', 'v:lua.vim.lsp.omnifunc')
 
-  -- Mappings.
   -- See `:help vim.lsp.*` for documentation on any of the below functions
-  vim.api.nvim_buf_set_keymap(bufnr, 'n', 'gD', '<cmd>lua vim.lsp.buf.declaration()<CR>', opts)
-  vim.api.nvim_buf_set_keymap(bufnr, 'n', 'gd', '<cmd>lua vim.lsp.buf.definition()<CR>', opts)
-  vim.api.nvim_buf_set_keymap(bufnr, 'n', 'K', '<cmd>lua vim.lsp.buf.hover()<CR>', opts)
-  vim.api.nvim_buf_set_keymap(bufnr, 'n', 'gi', '<cmd>lua vim.lsp.buf.implementation()<CR>', opts)
-  vim.api.nvim_buf_set_keymap(bufnr, 'n', '<C-k>', '<cmd>lua vim.lsp.buf.signature_help()<CR>', opts)
-  vim.api.nvim_buf_set_keymap(bufnr, 'n', '<space>wa', '<cmd>lua vim.lsp.buf.add_workspace_folder()<CR>', opts)
-  vim.api.nvim_buf_set_keymap(bufnr, 'n', '<space>wr', '<cmd>lua vim.lsp.buf.remove_workspace_folder()<CR>', opts)
-  vim.api.nvim_buf_set_keymap(bufnr, 'n', '<space>wl', '<cmd>lua print(vim.inspect(vim.lsp.buf.list_workspace_folders()))<CR>', opts)
-  vim.api.nvim_buf_set_keymap(bufnr, 'n', '<space>D', '<cmd>lua vim.lsp.buf.type_definition()<CR>', opts)
-  vim.api.nvim_buf_set_keymap(bufnr, 'n', '<space>rn', '<cmd>lua vim.lsp.buf.rename()<CR>', opts)
-  vim.api.nvim_buf_set_keymap(bufnr, 'n', '<space>ca', '<cmd>lua vim.lsp.buf.code_action()<CR>', opts)
-  vim.api.nvim_buf_set_keymap(bufnr, 'n', 'gr', '<cmd>lua vim.lsp.buf.references()<CR>', opts)
-  vim.api.nvim_buf_set_keymap(bufnr, 'n', '<space>f', '<cmd>lua vim.lsp.buf.formatting()<CR>', opts)
+  map(bufnr, 'n', 'gD', '<cmd>lua vim.lsp.buf.declaration()<CR>', opts)
+  map(bufnr, 'n', 'gi', '<cmd>lua vim.lsp.buf.implementation()<CR>', opts)
+  map(bufnr, 'n', 'cD', '<cmd>lua vim.lsp.buf.type_definition()<CR>', opts)
+  map(bufnr, 'n', 'gr', '<cmd>lua vim.lsp.buf.references()<CR>', opts)
+  map(bufnr, 'n', 'gf', '<cmd>lua vim.lsp.buf.formatting()<CR>', opts)
+  map(bufnr, 'n', '<space>wa', '<cmd>lua vim.lsp.buf.add_workspace_folder()<CR>', opts)
+  map(bufnr, 'n', '<space>wr', '<cmd>lua vim.lsp.buf.remove_workspace_folder()<CR>', opts)
+  map(bufnr, 'n', '<space>wl', '<cmd>lua print(vim.inspect(vim.lsp.buf.list_workspace_folders()))<CR>', opts)
+
+  if not vim.fn.exists(':Lspsaga') then
+    map(bufnr, 'n', 'gh', '<cmd>lua vim.lsp.buf.hover()<CR>', opts)
+    map(bufnr, 'n', 'ca', '<cmd>lua vim.lsp.buf.code_action()<CR>', opts)
+    map(bufnr, 'n', '<C-k>', '<cmd>lua vim.lsp.buf.signature_help()<CR>', opts)
+    map(bufnr, 'n', 'cr', '<cmd>lua vim.lsp.buf.rename()<CR>', opts)
+    map(bufnr, 'n', 'gd', '<cmd>lua vim.lsp.buf.definition()<CR>', opts)
+
+    -- See `:help vim.diagnostic.*` for documentation on any of the below functions
+    map(bufnr, 'n', 'go', '<cmd>lua vim.diagnostic.open_float()<CR>', opts)
+    map(bufnr, 'n', '[d', '<cmd>lua vim.diagnostic.goto_prev()<CR>', opts)
+    map(bufnr, 'n', ']d', '<cmd>lua vim.diagnostic.goto_next()<CR>', opts)
+    map(bufnr, 'n', '<space>q', '<cmd>lua vim.diagnostic.setloclist()<CR>', opts)
+  else
+    -- See https://github.com/tami5/lspsaga.nvim
+    map(bufnr, 'n', 'K', '<cmd>lua require("lspsaga.hover").render_hover_doc(<CR>', opts)
+
+    map(bufnr, 'n', 'gh', '<cmd>lua require("lspsaga.provider").lsp_finder()<CR>', opts)
+    map(bufnr, 'n', 'ca', '<cmd>lua require("lspsaga.codeaction").code_action()<CR>', opts)
+    map(bufnr, 'v', 'ca', '<cmd><C-U>lua require("lspsaga.codeaction").range_code_action()<CR>', opts)
+    map(bufnr, 'n', '<C-k>', '<cmd>lua require("lspsaga.signaturehelp").signature_help()<CR>', opts)
+    map(bufnr, "n", "cr", '<cmd>lua require("lspsaga.rename").rename()<cr>', opts)
+    map(bufnr, 'n', 'gd', '<cmd>lua require"lspsaga.provider".preview_definition()<CR>', opts)
+
+    map(bufnr, "n", "go", "<cmd>Lspsaga show_line_diagnostics<cr>", opts)
+    map(bufnr, "n", "[d", "<cmd>Lspsaga diagnostic_jump_prev<cr>", opts)
+    map(bufnr, "n", "]d", "<cmd>Lspsaga diagnostic_jump_next<cr>", opts)
+
+    -- scroll up hover doc
+    map(bufnr, "n", "<C-b>", "<cmd>lua require('lspsaga.action').smart_scroll_with_saga(-1, '<c-b>')<cr>", opts)
+    -- scroll down hover doc or scroll in definition preview
+    map(bufnr, "n", "<C-f>", "<cmd>lua require('lspsaga.action').smart_scroll_with_saga(1, '<c-f>')<cr>", opts)
+  end
+
 end
 
 -- config that activates keymaps and enables snippet support
