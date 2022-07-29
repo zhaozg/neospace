c3d03083364a773dac7463f345c5b0c3ed8672b3
zhaozg <zhaozg@gmail.com>
2022-07-29 22:25:36 +0800

    update theme layer

--- layers/theme.lua
+++ layers/theme.lua
@@ -1,22 +1,9 @@
-local g = vim.g
-
 return {
   {
-    'morhetz/gruvbox',
-    init = function()
-      -- gruvbox {
-      g.gruvbox_bold=1
-      g.gruvbox_italic=1
-      g.gruvbox_undercurl=1
-      g.gruvbox_underline=1
-      g.gruvbox_inverse=1
-      g.gruvbox_improved_strings=1
-      g.gruvbox_improved_warnings=1
-      g.gruvbox_termcolors=256
-      g.gruvbox_invert_indent_guides=0
-      -- }
-    end,
+    'ellisonleao/gruvbox.nvim',
     config = function()
+      require("gruvbox").setup()
+
       vim.cmd [[colorscheme gruvbox]]
     end
   }
--- tests/user/async.lua
+++ tests/user/async.lua
@@ -79,30 +79,6 @@ use {
 }
 
 use {
-  "gruvbox-community/gruvbox",
-  init = function()
-    vim.g.gruvbox_bold                 = 1
-    vim.g.gruvbox_italic               = 1
-    vim.g.gruvbox_transparent_bg       = 1
-    vim.g.gruvbox_underline            = 1
-    vim.g.gruvbox_undercurl            = 1
-    vim.g.gruvbox_termcolors           = 256
-    vim.g.gruvbox_contrast_dark        = "medium"
-    vim.g.gruvbox_contrast_light       = "medium"
-    vim.g.gruvbox_italicize_comments   = 1
-    vim.g.gruvbox_italicize_strings    = 1
-    vim.g.gruvbox_invert_selection     = 0
-    vim.g.gruvbox_invert_signs         = 0
-    vim.g.gruvbox_invert_indent_guides = 0
-    vim.g.gruvbox_invert_tabline       = 0
-    vim.g.gruvbox_improved_strings     = 1
-    vim.g.gruvbox_improved_warnings    = 1
-    vim.g.gruvbox_guisp_fallback       = 1
-  end,
-  config = function() vim.api.nvim_command("colorscheme gruvbox") end,
-}
-
-use {
   "ziglang/zig.vim",
   init = function() vim.g.zig_fmt_autosave = false end,
 }
