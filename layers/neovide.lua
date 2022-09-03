5e1c4e80e8dede6f196409a09ae1c72727887695
zhaozg <zhaozg@gmail.com>
2022-09-03 11:08:53 +0800

    update neovide layer
    - update copy/pasta with system clipboards
    - <leader>tw toggle fullscreen
    - <M--> <M-=> <M-0> to decrease, increase, reset font size

--- layers/neovide.lua
+++ layers/neovide.lua
@@ -1,7 +1,10 @@
 local vim = vim
-local map = require'neospace'.map
+local map = require 'neospace'.map
 
-vim.o.guifont = "Hack Nerd Font:h19"
+if not vim.fn.exists('g:neovide') then
+  return
+end
+vim.g.neovide_input_macos_alt_is_meta = true
 vim.g.neovide_remember_window_size = true
 vim.g.neovide_fullscreen = true
 vim.g.neovide_no_idle = false
@@ -14,11 +17,56 @@ vim.g.neovide_cursor_antialiasing = false
 vim.g.neovide_cursor_vfx_mode = ""
 vim.g.neovide_input_use_logo = true
 
+vim.g.gui_font_default_size = 19
+vim.g.gui_font_size = vim.g.gui_font_default_size
+vim.g.gui_font_face = "Hack Nerd Font"
+
+RefreshGuiFont = function()
+  vim.opt.guifont = string.format("%s:h%s", vim.g.gui_font_face, vim.g.gui_font_size)
+end
+
+ResizeGuiFont = function(delta)
+  vim.g.gui_font_size = vim.g.gui_font_size + delta
+  RefreshGuiFont()
+end
+
+ResetGuiFont = function()
+  vim.g.gui_font_size = vim.g.gui_font_default_size
+  RefreshGuiFont()
+end
+
+-- Call function on startup to set default value
+ResetGuiFont()
+
+-- Dynamic change font size
+
+map({ 'n', 'i' }, "<A-=>", "", {
+  callback = function() ResizeGuiFont(0.5) end
+})
+map({ 'n', 'i' }, "<A-->", "", {
+  callback = function() ResizeGuiFont(-0.5) end
+})
+map({ 'n', 'i' }, "<A-0>", "", {
+  callback = function() ResetGuiFont() end
+})
+
+-- system clipboard {{{
+vim.g.neovide_input_use_logo = true
 vim.cmd("set clipboard+=unnamedplus")
 
--- system clipboard
+-- copy
 map("", "<D-c>", '"+y')
+
+-- pasta
 map("n", "<D-v>", '"+p')
-map({"i", "c"}, "<D-v>", '<c-r>+')
---use <c-r> to insert original character without triggering things like auto-pairs
-map("i", "<D-r>", "<c-v>")
+map("i", "<D-v>", '<Esc>"+pa')
+map("c", "<D-v>", '<c-r>+', {})
+map("t", "<D-v>", '<c-\\><c-n>"+pa')
+
+-- undo
+map("n", "<D-z>", '"u')
+map("i", "<D-z>", '<Esc>ua')
+
+-- system clipboard }}}
+
+map("n", "<leader>tw", '<cmd>let g:neovide_fullscreen = !g:neovide_fullscreen<cr>', {})
