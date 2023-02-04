e2a95b16bbf03ad52778ef9dd59c141be2f43fdd
zhaozg <zhaozg@gmail.com>
2023-02-04 15:58:02 +0800

    update diffview.nvim config

--- layers/text.lua
+++ layers/text.lua
@@ -95,10 +95,30 @@ return {
       require("diffview").setup({
         diff_binaries = false, -- Show diffs for binaries
         use_icons = true, -- Requires nvim-web-devicons
+
+        view = {
+          -- For more info, see |diffview-config-view.x.layout|.
+          default = {
+            -- Config for changed files, and staged files in diff views.
+            layout = "diff2_vertical",
+            winbar_info = false, -- See |diffview-config-view.x.winbar_info|
+          },
+          merge_tool = {
+            -- Config for conflicted files in diff views during a merge or rebase.
+            layout = "diff3_mixed",
+            disable_diagnostics = true, -- Temporarily disable diagnostics for conflict buffers while in the view.
+            winbar_info = true, -- See |diffview-config-view.x.winbar_info|
+          },
+          file_history = {
+            -- Config for changed files in file history views.
+            layout = "diff2_horizontal",
+            winbar_info = false, -- See |diffview-config-view.x.winbar_info|
+          },
+        },
         file_panel = {
           win_config = {
             position = "left", -- One of 'left', 'right', 'top', 'bottom'
-            width = 35, -- Only applies when position is 'left' or 'right'
+            width = 25, -- Only applies when position is 'left' or 'right'
             height = 10, -- Only applies when position is 'top' or 'bottom'
           },
         },
