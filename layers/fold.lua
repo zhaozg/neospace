354de62aa7e1d5afe0f2fff5643c1103436b6bfb
zhaozg <zhaozg@gmail.com>
2023-03-02 21:53:19 +0800

    update fold layer, remove `pretty-fold.nvim`

--- layers/fold.lua
+++ layers/fold.lua
@@ -1,21 +1,49 @@
+local vim = vim
+
 return {
   {
     "kevinhwang91/nvim-ufo",
+    after = {
+      'kevinhwang91/promise-async',
+      'nvim-treesitter/nvim-treesitter'
+    },
     config = function()
+      local handler = function(virtText, lnum, endLnum, width, truncate)
+          local newVirtText = {}
+          local suffix = ('  *** ***  %d '):format(endLnum - lnum)
+          local sufWidth = vim.fn.strdisplaywidth(suffix)
+          local targetWidth = width - sufWidth
+          local curWidth = 0
+          for _, chunk in ipairs(virtText) do
+              local chunkText = chunk[1]
+              local chunkWidth = vim.fn.strdisplaywidth(chunkText)
+              if targetWidth > curWidth + chunkWidth then
+                  table.insert(newVirtText, chunk)
+              else
+                  chunkText = truncate(chunkText, targetWidth - curWidth)
+                  local hlGroup = chunk[2]
+                  table.insert(newVirtText, {chunkText, hlGroup})
+                  chunkWidth = vim.fn.strdisplaywidth(chunkText)
+                  -- str width returned from truncate() may less than 2nd argument, need padding
+                  if curWidth + chunkWidth < targetWidth then
+                      suffix = suffix .. (' '):rep(targetWidth - curWidth - chunkWidth)
+                  end
+                  break
+              end
+              curWidth = curWidth + chunkWidth
+          end
+          table.insert(newVirtText, {suffix, 'MoreMsg'})
+          return newVirtText
+      end
       require("ufo").setup({
         provider_selector = function()
           return { "treesitter", "indent" }
         end,
+        fold_virt_text_handler = handler,
       })
       -- Using ufo provider need remap `zR` and `zM`. If Neovim is 0.6.1, remap yourself
       vim.keymap.set("n", "zR", require("ufo").openAllFolds)
       vim.keymap.set("n", "zM", require("ufo").closeAllFolds)
     end,
-  },
-  {
-    "anuvyklack/pretty-fold.nvim",
-    config = function()
-      require("pretty-fold").setup()
-    end,
-  },
+  }
 }
