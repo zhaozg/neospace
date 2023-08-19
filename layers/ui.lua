e5b419e7d31d74b6311ae770bba28a2be02fb741
zhaozg <zhaozg@gmail.com>
2023-08-19 10:12:40 +0800

    chore(ui): update nvim-tree settings

--- layers/ui.lua
+++ layers/ui.lua
@@ -118,44 +118,11 @@ return  {
       require("nvim-tree").setup({
         -- disables netrw completely
         disable_netrw = true,
-        -- hijack netrw window on startup
-        hijack_netrw = true,
-        -- hijack the cursor in the tree to put it at the start of the filename
-        hijack_cursor = false,
-        -- updates the root directory of the tree on `DirChanged` (when your run `:cd` usually)
-        update_cwd = false,
-        -- update the focused file on `BufEnter`, un-collapses the folders recursively until it finds the file
-        update_focused_file = {
-          -- enables the feature
-          enable = false,
-          -- update the root directory of the tree to the one of the folder containing the file if the file is not under the current root directory
-          -- only relevant when `update_focused_file.enable` is true
-          update_cwd = false,
-          -- list of buffer names / filetypes that will not update the cwd if the file isn't found under the current root directory
-          -- only relevant when `update_focused_file.update_cwd` is true and `update_focused_file.enable` is true
-          ignore_list = {},
-        },
-        -- configuration options for the system open command (`s` in the tree by default)
-        system_open = {
-          -- the command to run this, leaving nil should work in most cases
-          cmd = nil,
-          -- the command arguments as a list
-          args = {},
-        },
-
         view = {
           -- width of the window, can be either a number (columns) or a string in `%`, for left or right side placement
           width = 30,
           -- side of the tree, can be one of 'left' | 'right' | 'top' | 'bottom'
           side = "left",
-          -- if true the tree will resize itself after opening a file
-          mappings = {
-            -- custom only false will merge the list with the default mappings
-            -- if true, it will only use your list to set the mappings
-            custom_only = false,
-            -- list of mappings to set on the tree manually
-            list = {},
-          },
         },
         renderer = {
           add_trailing = true,
