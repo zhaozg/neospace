5150d4b564104d2a871c1e1ff9ee9051ee9dad2d
zhaozg <zhaozg@gmail.com>
2023-08-27 20:15:15 +0800

    update code layer for nvim-autopairs modules

--- layers/code.lua
+++ layers/code.lua
@@ -1,3 +1,4 @@
+
 local vim = vim
 local g = vim.g
 local map = require("neospace").map
@@ -237,6 +238,44 @@ return {
   {
     "windwp/nvim-autopairs",
     config = function()
+      local cmp_autopairs = require('nvim-autopairs.completion.cmp')
+      local handlers = require('nvim-autopairs.completion.handlers')
+      local cmp = require('cmp')
+      cmp.event:on(
+        'confirm_done',
+        cmp_autopairs.on_confirm_done({
+          filetypes = {
+            -- "*" is a alias to all filetypes
+            ["*"] = {
+              ["("] = {
+                kind = {
+                  cmp.lsp.CompletionItemKind.Function,
+                  cmp.lsp.CompletionItemKind.Method,
+                },
+                handler = handlers["*"]
+              }
+            },
+            lua = {
+              ["("] = {
+                kind = {
+                  cmp.lsp.CompletionItemKind.Function,
+                  cmp.lsp.CompletionItemKind.Method
+                },
+                -- @param char string
+                -- @param item table item completion
+                -- @param bufnr number buffer number
+                -- @param rules table
+                -- @param commit_character table<string>
+                -- handler = function(char, item, bufnr, rules, commit_character)
+                --   -- Your handler function. Inpect with print(vim.inspect{char, item, bufnr, rules, commit_character})
+                -- end
+              }
+            },
+            -- Disable for tex
+            tex = false
+          }
+        })
+      )
       local npairs = require("nvim-autopairs")
       npairs.setup({
         check_ts = false,
