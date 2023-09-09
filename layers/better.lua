d53f5cf4ea25a823b17c2597d042ff0f50e0e19a
zhaozg <zhaozg@gmail.com>
2023-09-09 08:59:39 +0800

    add bigfile.nvim in better layer

--- layers/better.lua
+++ layers/better.lua
@@ -19,5 +19,28 @@ return {
       require"nvim-mapper".setup({})
     end,
     before = "nvim-telescope/telescope.nvim"
+  },
+  {
+    "LunarVim/bigfile.nvim",
+    config = function()
+      require("bigfile").setup {
+        filesize = 1,
+
+        pattern = function(bufnr, filesize_mib)
+          local file_contents = vim.fn.readfile(vim.api.nvim_buf_get_name(bufnr))
+          local file_length = #file_contents
+          if file_length > 10000 or filesize_mib > 1 then
+            return true
+          end
+        end,
+
+        features = {
+          "indent_blankline",
+          "treesitter",
+          "matchparen",
+          "vimopts",
+        },
+      }
+    end
   }
 }
