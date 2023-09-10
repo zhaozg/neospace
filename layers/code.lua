175c9668fe482516403e812ec335a234bef4172e
zhaozg <zhaozg@gmail.com>
2023-09-10 13:05:25 +0800

    add hrsh7th/cmp-nvim-lsp-signature-help modules

--- layers/code.lua
+++ layers/code.lua
@@ -73,6 +73,7 @@ return {
   "hrsh7th/cmp-path",
   "hrsh7th/cmp-cmdline",
   "hrsh7th/cmp-emoji",
+  "hrsh7th/cmp-nvim-lsp-signature-help",
 
   "L3MON4D3/LuaSnip",
   "saadparwaiz1/cmp_luasnip",
@@ -204,6 +205,7 @@ return {
           { name = "doxygen" },
           { name = "spell" },
           { name = "emoji" },
+          { name = 'nvim_lsp_signature_help' },
 
           { name = "buffer" },
           { name = "path" },
