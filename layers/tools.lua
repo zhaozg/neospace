a8ab592d55af242e1800c5b0e19d3ec31d5f680e
zhaozg <zhaozg@gmail.com>
2023-12-05 13:40:35 +0800

    feat: add rest-nvim/rest.nvim in tools layer

--- layers/tools.lua
+++ layers/tools.lua
@@ -1,5 +1,22 @@
 return {
   {
+    "rest-nvim/rest.nvim",
+    after = { "nvim-lua/plenary.nvim" },
+    config = function()
+      local rest = require('rest-nvim')
+      rest.setup({
+        result = {
+          show_url = true,
+          show_curl_command = true,
+          show_http_info = true,
+          show_headers = true,
+        }
+      })
+
+      vim.keymap.set('n', '<CR>', rest.run)
+    end
+  },
+  {
     "zhaozg/taskwarrior.nvim",
     on = "Task",
     after = "nvim-telescope/telescope.nvim",
