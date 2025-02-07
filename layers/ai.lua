9978ac0a92ba3ce016d1bfad1bf19192d969da3a
zhaozg <zhaozg@gmail.com>
2025-02-08 00:16:32 +0800

    feat: use telescope for action palette in codecompanion
    - Added display configuration with action_palette settings including width, height, prompt, provider, and options.

--- layers/ai.lua
+++ layers/ai.lua
@@ -19,6 +19,20 @@ return {
     },
     config = function ()
       require("codecompanion").setup({
+        display = {
+          action_palette = {
+            width = 95,
+            height = 10,
+            prompt = "Prompt ", -- Prompt used for interactive LLM calls
+            provider = "telescope", -- default|telescope|mini_pick
+            opts = {
+              -- Show the default actions in the action palette?
+              show_default_actions = true,
+              -- Show the default prompt library in the action palette?
+              show_default_prompt_library = true,
+            },
+          },
+        },
         opts = {
           log_level = "DEBUG",
         },
